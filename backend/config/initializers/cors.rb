c3528f62048e784e1a3999d7448f6eeaf221f30c
Vincent Wu <sheep0406@hotmail.com>
2025-05-13 11:39:52 +0800

    fix: init params

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -9,6 +9,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
 
+    allowed_origins = []
     if Rails.env.development?
       allowed_origins += [
         'http://localhost:3000',
