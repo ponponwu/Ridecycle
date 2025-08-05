44f7bde986c4cf917eb79ef109d08d28c95efe51
Vincent Wu <sheep0406@hotmail.com>
2025-08-05 17:13:40 +0800

    refactor(api): 簡化 MessageSerializer 的關聯輸出

--- backend/app/serializers/message_serializer.rb
+++ backend/app/serializers/message_serializer.rb
@@ -4,9 +4,44 @@ class MessageSerializer
 
   attributes :id, :content, :created_at, :read_at, :is_offer, :offer_amount, :offer_status
 
-  belongs_to :sender, serializer: :user, record_type: :user
-  belongs_to :recipient, serializer: :user, record_type: :user
-  belongs_to :bicycle, serializer: :bicycle, record_type: :bicycle
+  # 直接提供 sender 和 recipient 資訊，避免 JSON:API 關聯複雜性
+  attribute :sender do |object|
+    if object.sender
+      {
+        id: object.sender.id.to_s,
+        name: object.sender.name,
+        email: object.sender.email,
+        avatar_url: object.sender.avatar_url
+      }
+    else
+      nil
+    end
+  end
+
+  attribute :recipient do |object|
+    if object.recipient
+      {
+        id: object.recipient.id.to_s,
+        name: object.recipient.name,
+        email: object.recipient.email,
+        avatar_url: object.recipient.avatar_url
+      }
+    else
+      nil
+    end
+  end
+
+  attribute :bicycle do |object|
+    if object.bicycle
+      {
+        id: object.bicycle.id.to_s,
+        title: object.bicycle.title,
+        price: object.bicycle.price
+      }
+    else
+      nil
+    end
+  end
 
   attribute :sender_id do |object|
     object.sender_id.to_s
